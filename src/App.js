e6581dfc2323431ccaf2f1f7533d845883103bce
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-06-14 19:24:26 +0300

    add react files for classroom

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import About from "./About";
+
+export const Introduction = ()=>{
+  return <h2>What is React?</h2>;
+  
+};
+const App= ()=>{
+  return (
+    <div>
+      <h1>Introduction to React.JS</h1>
+      <Introduction/>
+<About/>
+    </div>
+  )
+}
+export default App;
